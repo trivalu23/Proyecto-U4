465d5ff5800417de12b6fdaa2d9fa3f3cd882ed8
trivalu23 <niccondooriasto@crackthecode.la>
2025-11-14 19:25:07 -0500

    Delete

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAOm_dVhveLM2P-nbaMzk19gxvMsqtgTsc",
-  authDomain: "fitlife-u4.firebaseapp.com",
-  projectId: "fitlife-u4",
-  storageBucket: "fitlife-u4.firebasestorage.app",
-  messagingSenderId: "497724041285",
-  appId: "1:497724041285:web:e6c8901cb03e3ca3269b2c",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const provider = new GoogleAuthProvider();
